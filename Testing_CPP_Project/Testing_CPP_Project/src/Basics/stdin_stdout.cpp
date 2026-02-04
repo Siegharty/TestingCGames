9ca0b2310ab530e801119df64c3a44242aa1c73a
Siegharty <kevinncabreradv@gmail.com>
2026-02-04 00:15:52 -0300

    Inital configuration of project and leave some examples

---
+++ Testing_CPP_Project/Testing_CPP_Project/src/Basics/stdin_stdout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void main() {
+    int velocity = 1;
+    std::string i_am_seeing = "Yes";
+    bool watching_video = true;
+    double what_time_it_is = 23.31;
+    char sleepy = 'y';
+
+    std::cout << velocity << i_am_seeing << watching_video << what_time_it_is << sleepy << std::endl;
+}
